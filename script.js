21a27fba0a35374614c6cc1b62dd170274907ea7
Loris Pozzuoli <lorispozzuoli@gmail.com>
2021-11-19 14:49:34 +0100

    finito wrapper plp fare link a pagina e pdp

---
+++ script.js
@@ -0,0 +1,36 @@
+fetch("https://assets.fc-dev.instore.oakley.com/assets/products/products.json")
+  .then((response) => response.json())
+  .then((products) => {
+    function populateAds(products) {
+      let productsWrapper = document.querySelector("#products-wrapper");
+
+      products.forEach((product) => {
+        let card = document.createElement("div");
+
+        let chip;
+
+        if (product.availability.stock > 0) {
+          chip = `<span class="chip"> in stock </span>`;
+        } else {
+          chip = `<span class="chip"> out of stock </span>`;
+        }
+
+        card.innerHTML = `
+        <div class="card" key=${product.UPC}>
+        <a class="cardlink" href={/product/${product.UPC}}>
+            <img class="cardmedia" src="https://via.placeholder.com/350" />
+            <div class="cardcontent">
+                <h5 class="typography">${product.name}</h5>
+                <p class="typographyprice">$ ${product.price.current.value}</p>
+                <p class="typographystock">
+                    ${chip}
+                </p>
+            </div>
+        </a>
+    </div>
+            `;
+        productsWrapper.appendChild(card);
+      });
+    }
+    populateAds(products);
+  });
